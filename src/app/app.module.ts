174b141dd08c31714c42524d4671ac874e0b4200
unknown <admin.prashant@1B6QYM2.in.decos.com>
2018-07-31 10:31:29 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +25,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    SpeechRecognition,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
