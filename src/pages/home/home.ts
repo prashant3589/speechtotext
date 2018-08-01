4ba9c41478ae0fcdcf35d3a758cc54fee3968dc1
unknown <admin.prashant@1B6QYM2.in.decos.com>
2018-08-01 16:10:31 +0530

    UI and config changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,41 +11,67 @@ import { ChangeDetectorRef } from '@angular/core';
 export class HomePage {
   matches: String[];
   isRecording = false;
- 
-  constructor(public navCtrl: NavController, private speechRecognition: SpeechRecognition, private plt: Platform, private cd: ChangeDetectorRef)
-   {
+  match: string;
+  savecontent :String;
+  constructor(public navCtrl: NavController, private speechRecognition: SpeechRecognition, private plt: Platform, private cd: ChangeDetectorRef) {
     this.getPermission();
-    }
- 
+    this.savecontent='';    
+  }
+
   isIos() {
     return this.plt.is('ios');
   }
- 
+
   stopListening() {
     this.speechRecognition.stopListening().then(() => {
       this.isRecording = false;
     });
   }
- 
+
   getPermission() {
     this.speechRecognition.hasPermission()
       .then((hasPermission: boolean) => {
         if (!hasPermission) {
           this.speechRecognition.requestPermission();
-          this.startListening();  
         }
       });
   }
- 
+
   startListening() {
     let options = {
       language: 'en-US'
     }
     this.speechRecognition.startListening().subscribe(matches => {
+
       this.matches = matches;
+      if (this.matches.length > 0) {
+        this.match = matches[0];
+      }
+      else {
+        this.match = "";
+      }
       this.cd.detectChanges();
     });
     this.isRecording = true;
+
   }
- 
+  push()
+  {
+  if(!this.match)
+  {
+    this.match='';
+  }
+    this.savecontent=this.savecontent.concat(this.match);
+  }
+  pop()
+  {
+    if(this.savecontent.length>0)
+    {
+      this.savecontent.replace(this.match,'');
+      
+    }
+  }
+  save()
+  {}
+
 }
